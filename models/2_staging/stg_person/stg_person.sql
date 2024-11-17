bbab9ac10e965f1fc6c1009fe9a757d5268df069
Felipe Natanael <felipenatanael93@gmail.com>
2024-11-17 21:17:31 +0000

    Última revisão antes de criar as tabelas Fatos.

--- models/2_staging/stg_person/stg_person.sql
+++ models/2_staging/stg_person/stg_person.sql
@@ -1,13 +1,13 @@
 with 
     person as (
         select 
-        businessentityid
-        , persontype
-        , firstname
-        , middlename
-        , lastname
-        , TRIM(CONCAT(firstname, ' ', COALESCE(middlename, ''), ' ', lastname)) AS fullname
-        , modifieddate
+            businessentityid
+            , persontype
+            , firstname
+            , middlename
+            , lastname
+            , TRIM(CONCAT(firstname, ' ', COALESCE(middlename, ''), ' ', lastname)) AS fullname
+            , modifieddate
     from {{ source('stg_person', 'person') }}
     )
 
